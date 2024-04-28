7e4ff3f5a174d4c7af11629c46193056e90368ed
Lomiig <95091244+Lomiig@users.noreply.github.com>
2024-04-28 20:45:47 +0200

    Updated main file
    Added functions:
    -cleaner: deletes lone pixels
    -count: counts the number of non-empty pixels around the chosen pixel
    -sharpen: multiple transformations to sharpen an image
    -applywatershed: return a Mat which shows the result of the watershed algorithm after coloring
    
    Now applies the watershed algorithm instead of overlaying the previously found contours on top of the source image

--- Beginner.cpp
+++ Beginner.cpp
@@ -6,53 +6,340 @@
 using namespace cv;
 using namespace std;
 
+/*
+Removes lone pixels
+*/
+Mat cleaner(Mat);
+
+/*
+Counts how many pixels are different from 0 in a 3x3 area around pixel with coords (i,j)
+*/
+int count(Mat, int, int);
+
+/*
+Apply multiple transformations to return a sharpened image
+*/
+Mat sharpen(Mat);
+
+/*
+Create a Mat that you can imshow after a watershed transform
+*/
+Mat applywatershed(Mat, vector<vector<Point>>);
+
 int main() {
-	string image_path = "street1.png";
+
+	string image_path = "E:/photos/street3.png";
 	Mat Clrimage = imread(image_path, IMREAD_COLOR);
 	Mat image = imread(image_path, IMREAD_GRAYSCALE);
-	Mat blur,laplimg,combimg,thimg,dist,imgcont,out,comb_8u;
+	Mat  combimg, thimg, dist, out, sharp;
 
-	int ksize = 3;
-	int scaleF = 3;
-	int delta = 0;
-
-	
-	medianBlur(image, blur, 7);
-	Laplacian(blur, laplimg, -1, ksize,scaleF,delta);
-	threshold(laplimg, laplimg, 90, 255, THRESH_BINARY);
-
-	combimg = blur - laplimg;
+	combimg = sharpen(image);
+	sharp = sharpen(Clrimage);
 
 	threshold(combimg, thimg, 45, 255, THRESH_BINARY);
-
 	distanceTransform(thimg, dist, DIST_L2, 3);
 	normalize(dist, dist, 0, 1, NORM_MINMAX);
-	erode(dist, dist, Mat::ones(3, 3, CV_8U), Point(-1, -1),3);
-	threshold(dist, dist, 0.04, 1, THRESH_BINARY);
+	erode(dist, dist, Mat::ones(3, 3, CV_8U), Point(-1, -1), 3);
+	threshold(dist, dist, 0.08, 1, THRESH_BINARY);
 
 	Mat dist_8u;
 	dist.convertTo(dist_8u, CV_8U);
 
 	vector<vector<Point> > contours;
-	vector<Vec4i> hierarchy;
-	findContours(dist_8u, contours, hierarchy,RETR_LIST, CHAIN_APPROX_SIMPLE);
+	findContours(dist_8u, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
 
-	Mat markers = Mat::zeros(dist.size(), CV_8UC3);
+	Mat markers = Mat::zeros(dist.size(), CV_32S);
 	for (size_t i = 0; i < contours.size(); i++)
 	{
-		drawContours(markers, contours, static_cast<int>(i), Scalar(0, 255-(int)i, (int)i), -1);
+		drawContours(markers, contours, static_cast<int>(i), Scalar(static_cast<int>(i) + 1), -1);
 	}
 
-	Mat mark_out;
-	markers.convertTo(mark_out, image.type());
+	circle(markers, Point(5, 5), 3, Scalar(255), -1);
+	Mat markers8u;
+	markers.convertTo(markers8u, CV_8U, 10);
 
-	double alpha = 0.8; double beta;
-	beta = (1.0 - alpha);
-	addWeighted(mark_out, alpha, Clrimage, beta, 0.0, out);
-
+	watershed(sharp, markers);
+	out = applywatershed(markers, contours);
 	imshow("Out", out);
 
 	waitKey(0);
 
 	return 0;
+}
+
+
+Mat cleaner(Mat src) {
+	int channels = src.channels();
+	int nRows = src.rows;
+	int nCols = src.cols;
+	int i, j, nlife;
+	switch (channels) {
+		case 1: {
+			for (i = 0; i < nRows; i++) {
+				for (j = 0; j < nCols; j++) {
+					if (src.at<uchar>(i, j) != 0) {
+						nlife = count(src, i, j);
+						if (nlife <= 2 || nlife >= 8) {
+							src.at<uchar>(i, j) = 0;
+						}
+					}
+				}
+			}
+			break;
+		}
+		case 3: {
+			Mat_<Vec3b> _src = src;
+			for (i = 0; i < nRows; i++) {
+				for (j = 0; j < nCols; j++) {
+					if (_src(i, j)[0] != 0 || _src(i, j)[1] != 0 || _src(i, j)[2] != 0 ) {
+						nlife = count(src, i, j);
+						if (nlife <= 2 || nlife >= 8) {
+							_src(i, j)[0] = 0;
+							_src(i, j)[1] = 0;
+							_src(i, j)[2] = 0;
+						}
+					}
+				}
+			}
+			src = _src;
+			break;
+		}
+	}
+	return src;
+}
+
+int count(Mat src, int i, int j) {
+	int channels = src.channels();
+	int k, l;
+	int s = 0;
+	switch (channels) {
+		case 1: {
+			if (i == 0) {
+				if (j == 0) {
+					for (k = 0; k < 2; k++) {
+						for (l = 0; l < 2; l++) {
+							if (src.at<uchar>(i + k, j + l) != 0) {
+								s = s + 1;
+							}
+						}
+					}
+				}
+				else if (j == src.cols - 1) {
+					for (k = 0; k < 2; k++) {
+						for (l = -1; l < 1; l++) {
+							if (src.at<uchar>(i + k, j + l) != 0) {
+								s = s + 1;
+							}
+						}
+					}
+				}
+				else {
+					for (k = 0; k < 2; k++) {
+						for (l = -1; l < 2; l++) {
+							if (src.at<uchar>(i + k, j + l) != 0) {
+								s = s + 1;
+							}
+						}
+					}
+				}
+
+			}
+			else if (i == src.rows - 1) {
+				if (j == 0) {
+					for (k = -1; k < 1; k++) {
+						for (l = 0; l < 2; l++) {
+							if (src.at<uchar>(i + k, j + l) != 0) {
+								s = s + 1;
+							}
+						}
+					}
+				}
+				else if (j == src.cols - 1) {
+					for (k = -1; k < 1; k++) {
+						for (l = -1; l < 1; l++) {
+							if (src.at<uchar>(i + k, j + l) != 0) {
+								s = s + 1;
+							}
+						}
+					}
+				}
+				else {
+					for (k = -1; k < 1; k++) {
+						for (l = -1; l < 2; l++) {
+							if (src.at<uchar>(i + k, j + l) != 0) {
+								s = s + 1;
+							}
+						}
+					}
+				}
+			}
+			else if (j == 0) {
+				for (k = -1; k < 2; k++) {
+					for (l = 0; l < 2; l++) {
+						if (src.at<uchar>(i + k, j + l) != 0) {
+							s = s + 1;
+						}
+					}
+				}
+			}
+			else if (j == src.cols - 1) {
+				for (k = -1; k < 2; k++) {
+					for (l = -1; l < 1; l++) {
+						if (src.at<uchar>(i + k, j + l) != 0) {
+							s = s + 1;
+						}
+					}
+				}
+			}
+			else {
+				for (k = -1; k < 2; k++) {
+					for (l = -1; l < 2; l++) {
+						if (src.at<uchar>(i + k, j + l) != 0) {
+							s = s + 1;
+						}
+					}
+				}
+			}
+			break;
+		}
+		case 3: {
+			Mat_<Vec3b> _src = src;
+			if (i == 0) {
+				if (j == 0) {
+					for (k = 0; k < 2; k++) {
+						for (l = 0; l < 2; l++) {
+							if (_src(i+k, j+l)[0] != 0 || _src(i+k, j+l)[1] != 0 || _src(i+k, j+l)[2] != 0) {
+								s = s + 1;
+							}
+						}
+					}
+				}
+				else if (j == src.cols - 1) {
+					for (k = 0; k < 2; k++) {
+						for (l = -1; l < 1; l++) {
+							if (_src(i + k, j + l)[0] != 0 || _src(i + k, j + l)[1] != 0 || _src(i + k, j + l)[2] != 0) {
+								s = s + 1;
+							}
+						}
+					}
+				}
+				else {
+					for (k = 0; k < 2; k++) {
+						for (l = -1; l < 2; l++) {
+							if (_src(i + k, j + l)[0] != 0 || _src(i + k, j + l)[1] != 0 || _src(i + k, j + l)[2] != 0) {
+								s = s + 1;
+							}
+						}
+					}
+				}
+
+			}
+			else if (i == src.rows - 1) {
+				if (j == 0) {
+					for (k = -1; k < 1; k++) {
+						for (l = 0; l < 2; l++) {
+							if (_src(i + k, j + l)[0] != 0 || _src(i + k, j + l)[1] != 0 || _src(i + k, j + l)[2] != 0) {
+								s = s + 1;
+							}
+						}
+					}
+				}
+				else if (j == src.cols - 1) {
+					for (k = -1; k < 1; k++) {
+						for (l = -1; l < 1; l++) {
+							if (_src(i + k, j + l)[0] != 0 || _src(i + k, j + l)[1] != 0 || _src(i + k, j + l)[2] != 0) {
+								s = s + 1;
+							}
+						}
+					}
+				}
+				else {
+					for (k = -1; k < 1; k++) {
+						for (l = -1; l < 2; l++) {
+							if (_src(i + k, j + l)[0] != 0 || _src(i + k, j + l)[1] != 0 || _src(i + k, j + l)[2] != 0) {
+								s = s + 1;
+							}
+						}
+					}
+				}
+			}
+			else if (j == 0) {
+				for (k = -1; k < 2; k++) {
+					for (l = 0; l < 2; l++) {
+						if (_src(i + k, j + l)[0] != 0 || _src(i + k, j + l)[1] != 0 || _src(i + k, j + l)[2] != 0) {
+							s = s + 1;
+						}
+					}
+				}
+			}
+			else if (j == src.cols - 1) {
+				for (k = -1; k < 2; k++) {
+					for (l = -1; l < 1; l++) {
+						if (_src(i + k, j + l)[0] != 0 || _src(i + k, j + l)[1] != 0 || _src(i + k, j + l)[2] != 0) {
+							s = s + 1;
+						}
+					}
+				}
+			}
+			else {
+				for (k = -1; k < 2; k++) {
+					for (l = -1; l < 2; l++) {
+						if (_src(i + k, j + l)[0] != 0 || _src(i + k, j + l)[1] != 0 || _src(i + k, j + l)[2] != 0) {
+							s = s + 1;
+						}
+					}
+				}
+			}
+			break;
+		}
+	}
+	return s;
+}
+
+Mat sharpen(Mat src) {
+	Mat blur, laplimg, combimg, thimg, dilimg;
+	int ksize = 3;
+	int scaleF = 3;
+	int delta = 0;
+	medianBlur(src, blur, 5);
+	Laplacian(blur, laplimg, -1, ksize, scaleF, delta);
+	threshold(laplimg, thimg, 90, 255, THRESH_BINARY);
+
+	cleaner(thimg);
+
+	dilate(thimg, dilimg, Mat::ones(3, 3, CV_8U), Point(-1, -1), 1);
+
+	combimg = blur - dilimg; 
+	return combimg;
+}
+
+Mat applywatershed(Mat markers, vector<vector<Point> > contours){
+	Mat mark;
+	markers.convertTo(mark, CV_8U);
+	bitwise_not(mark, mark);
+
+	vector<Vec3b> colors;
+	for (size_t i = 0; i < contours.size(); i++)
+	{
+		int b = 0;
+		int g = 255 - 10 * (int)i;
+		int r = 10 * (int)i;
+
+		colors.push_back(Vec3b((uchar)b, (uchar)g, (uchar)r));
+	}
+
+	Mat dst = Mat::zeros(markers.size(), CV_8UC3);
+
+	for (int i = 0; i < markers.rows; i++)
+	{
+		for (int j = 0; j < markers.cols; j++)
+		{
+			int index = markers.at<int>(i, j);
+			if (index > 0 && index <= static_cast<int>(contours.size()))
+			{
+				dst.at<Vec3b>(i, j) = colors[index - 1];
+			}
+		}
+	}
+	return dst;
 }
